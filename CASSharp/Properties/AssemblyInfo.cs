3cf8818da04c3fbed63dac8d5ec0e319a1399ed8
bugbit <ohernandezba@gmail.com>
2019-11-12 13:19:19 +0100

    initial

--- CASSharp/Properties/AssemblyInfo.cs
+++ CASSharp/Properties/AssemblyInfo.cs
@@ -1,16 +1,43 @@
-﻿using System.Reflection;
+﻿#region LICENSE
+/*
+    MIT License
+
+    Copyright (c) 2018 Software free CAS Sharp
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+#endregion
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("CASSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CASSharp Edition Nieblita")]
+[assembly: AssemblyDescription("Software free CAS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CASSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("CAS Sharp")]
+[assembly: AssemblyCopyright("Copyright © 2018 Óscar Hernández Bañó")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- CASSharp/UI/MainForm.Designer.cs
+++ CASSharp/UI/MainForm.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.ssMain = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 24);
+            this.mnuMain.TabIndex = 1;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // ssMain
+            // 
+            this.ssMain.Location = new System.Drawing.Point(0, 428);
+            this.ssMain.Name = "ssMain";
+            this.ssMain.Size = new System.Drawing.Size(800, 22);
+            this.ssMain.TabIndex = 2;
+            this.ssMain.Text = "statusStrip1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ssMain);
+            this.Controls.Add(this.mnuMain);
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuMain;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.StatusStrip ssMain;
     }
 }
