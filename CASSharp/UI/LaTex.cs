937feb88626fed33f0028047c239fa97d180740e
bugbit <ohernandezba@gmail.com>
2019-11-15 15:56:58 +0100

    Update LaTex.cs
    UI Prompt

--- CASSharp/UI/LaTex.cs
+++ CASSharp/UI/LaTex.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,43 @@ namespace CASSharp.UI
     // https://github.com/mattleibow/skiasharp-samples/blob/master/Skia.WindowsDesktop.Demo/SkiaView.cs
     class LaTex : Control
     {
-        public string EnunciadoStr => string.Empty;
+        private MathPainter mPainter = new MathPainter(12);
+
+        public string LaTexStr
+        {
+            get => mPainter.LaTeX;
+            set
+            {
+                mPainter.LaTeX = value;
+
+                var pM = mPainter.Measure;
+
+                if (pM.HasValue)
+                    Size = pM.Value.Size.ToSize();
+            }
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            //new SkiaSharp.SKCanvas()
-            //v.
+            var pW = Width;
+            var pH = Height;
+
+            using (var pBmp = new Bitmap(pW, pH, PixelFormat.Format32bppPArgb))
+            {
+                var pData = pBmp.LockBits(new Rectangle(0, 0, pW, pH), ImageLockMode.WriteOnly, pBmp.PixelFormat);
+
+                using (var surface = SKSurface.Create(pW, pH, SKColorType.Argb4444, SKAlphaType.Premul, pData.Scan0, pW * 4))
+                {
+                    var pSKCanvas = surface.Canvas;
+
+                    mPainter.Draw(pSKCanvas, alignment: CSharpMath.Rendering.TextAlignment.Left);
+                }
+                pBmp.UnlockBits(pData);
+
+                e.Graphics.DrawImage(pBmp, new Rectangle(0, 0, Width, Height));
+            }
         }
     }
 }
