23d7abbd33bcb2606226f38f54f2017b4afa27b4
bugbit <ohernandezba@gmail.com>
2019-11-25 15:39:42 +0100

    PositionRange

---
+++ CASSharp/Comun/positionRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CASSharp.Comun
+{
+    class PositionRange : IComparable<PositionRange>
+    {
+        public int PosIni { get; set; }
+        public int PosFin { get; set; }
+
+        public int CompareTo(PositionRange obj)
+        {
+            var pCmp = PosIni - obj.PosIni;
+
+            if (pCmp == 0)
+                pCmp = PosFin - obj.PosFin;
+
+            return pCmp;
+        }
+    }
+}
