cbd0fdb3d2cf6fca8ab08c7d250ffc830c6a2e80
bugbit <ohernandezba@gmail.com>
2019-11-26 18:40:40 +0100

    Update STFormater.cs

--- CASSharp/Console/STFormater.cs
+++ CASSharp/Console/STFormater.cs
@@ -10,7 +10,7 @@ namespace CASSharp.Console
     {
         public const ConsoleColor ColorError = ConsoleColor.Red;
         public const ConsoleColor ColorNumber = ConsoleColor.DarkBlue;
-        public IDictionary<ESTType, ConsoleColor> TypesColores { get; } = new Dictionary<ESTType, ConsoleColor>
+        public static IDictionary<ESTType, ConsoleColor> TypesColores { get; } = new Dictionary<ESTType, ConsoleColor>
         {
             [ESTType.Error] = ColorError,
             [ESTType.Numeric] = ColorNumber
@@ -19,11 +19,19 @@ namespace CASSharp.Console
         public SortedSet<TextFormat> Format(LinkedList<STBase> argSts)
         {
             var pFmt = new SortedSet<TextFormat>();
-            var pSt = argSts.First;
 
-            //pSt.Equals
+            Format(pFmt, argSts);
 
             return pFmt;
+        }
+
+        private void Format(SortedSet<TextFormat> argFmt, LinkedList<STBase> argSts)
+        {
+            foreach (var pSt in argSts)
+            {
+                if (TypesColores.TryGetValue(pSt.Type, out ConsoleColor argColor))
+                    argFmt.Add(new TextFormat { Position = pSt.Position, BackColor = argColor });
+            }
         }
     }
 }
