b146b2aeacb57ac64610da30a307822cfcb0f785
bugbit <ohernandezba@gmail.com>
2020-01-07 18:50:37 +0100

    winforms

--- CASSharp/CASSharp.WinForms/FrmMain.Designer.cs
+++ CASSharp/CASSharp.WinForms/FrmMain.Designer.cs
@@ -1,4 +1,31 @@
-﻿namespace CASSharp.WinForms
+﻿#region LICENSE
+/*
+    MIT License
+
+    Copyright (c) 2018 Software free CAS Sharp
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+#endregion
+
+namespace CASSharp.WinForms
 {
     partial class FrmMain
     {
@@ -28,12 +55,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lyBoard = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lyBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lyBoard
+            // 
+            this.lyBoard.Controls.Add(this.lbHeader);
+            this.lyBoard.Controls.Add(this.label1);
+            this.lyBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lyBoard.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.lyBoard.Location = new System.Drawing.Point(0, 0);
+            this.lyBoard.Name = "lyBoard";
+            this.lyBoard.Size = new System.Drawing.Size(800, 450);
+            this.lyBoard.TabIndex = 0;
+            // 
+            // lbHeader
+            // 
+            this.lbHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbHeader.AutoSize = true;
+            this.lbHeader.Location = new System.Drawing.Point(3, 0);
+            this.lbHeader.Name = "lbHeader";
+            this.lbHeader.Padding = new System.Windows.Forms.Padding(6);
+            this.lbHeader.Size = new System.Drawing.Size(47, 25);
+            this.lbHeader.TabIndex = 0;
+            this.lbHeader.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lyBoard);
+            this.Name = "FrmMain";
             this.Text = "FrmMain";
+            this.lyBoard.ResumeLayout(false);
+            this.lyBoard.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel lyBoard;
+        private System.Windows.Forms.Label lbHeader;
+        private System.Windows.Forms.Label label1;
     }
 }
